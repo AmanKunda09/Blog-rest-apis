d0ad975de38c8cdc1fc867bfa0ba448a0886edee
aman <amankunda9@gmail.com>
2024-05-24 05:14:01 +0530

    adding database auth

--- src/main/java/com/aman/blog/blogrestapi/config/SecurityConfig.java
+++ src/main/java/com/aman/blog/blogrestapi/config/SecurityConfig.java
@@ -3,15 +3,15 @@ package com.aman.blog.blogrestapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,9 +19,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig{
 
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -34,18 +45,18 @@ public class SecurityConfig{
                 ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails aman= User.builder()
-                .username("aman")
-                .password(passwordEncoder().encode("aman12"))
-                .roles("USER")
-                .build();
-        UserDetails admin=User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(aman,admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails aman= User.builder()
+//                .username("aman")
+//                .password(passwordEncoder().encode("aman12"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin=User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(aman,admin);
+//    }
 }
